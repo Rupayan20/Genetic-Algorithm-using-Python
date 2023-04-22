64a4858ae469fea8f1c38459e23a2bc21d927d14
Rupayan Dutta <102848153+Rupayan20@users.noreply.github.com>
2023-04-22 22:54:21 +0530

    Update python.py

--- python.py
+++ python.py
@@ -72,7 +72,8 @@ for z in sorted_Z:
     print(z)
     
 
-# Multipoint Crossover
+# Multipoint Crossover for 1st Generation-
+
 def crossover(parents, num_offsprings):
     offsprings = []
     for i in range(num_offsprings):
@@ -82,20 +83,51 @@ def crossover(parents, num_offsprings):
         offsprings.append(offspring)
     return offsprings
 
-parents = [[6.69,  4.35, 53  , 18.28], [17.75,  7.68, 38  ,  1.72], [19.56, 13.41, 51  , 17.98], [10.82,  8.37, 52  , 22.45]]
-num_offsprings = 35
+parents = [[ 12.73,  16.66,   0.76,  58.69, 464.  , 152.  , 213.  ,   0.76,
+         0.62], [ 19.05,  11.89,   1.17,  85.66, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [  7.61,  14.09,   0.88,  69.42, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 17.47,  24.14,   0.52,  80.7 , 453.  , 119.  , 137.  ,   0.66,
+         0.61], [  8.59,  11.39,   0.64,  73.54, 471.  ,  86.  , 191.  ,   0.46,
+         0.39], [ 16.09,  11.63,   0.78,  59.78, 451.  , 108.  , 183.  ,   1.37,
+         1.19], [ 15.38,   3.18,   0.7 ,  72.97, 461.  ,  70.  , 194.  ,   0.57,
+         1.23], [ 18.09,   7.77,   0.66,  72.22, 483.  , 139.  ,  44.  ,   0.52,
+         1.21], [ 10.34,   7.46,   0.72,  77.79, 453.  ,  38.  , 242.  ,   0.16,
+         0.33], [ 16.62,  13.64,   0.87,  81.75, 416.  , 103.  , 175.  ,   1.84,
+         0.35], [ 18.47,  24.81,   1.17,  73.57, 403.  , 113.  , 169.  ,   0.62,
+         0.72], [ 15.02,  21.09,   0.96,  65.5 , 480.  ,  78.  , 125.  ,   0.78,
+         0.57], [ 15.84,   5.98,   1.08,  54.08, 462.  , 121.  , 105.  ,   1.44,
+         0.57], [  6.4 ,  10.13,   0.54,  59.24, 387.  , 129.  , 202.  ,   0.56,
+         0.08], [  7.67,   7.03,   0.67,  84.59, 443.  , 150.  ,  53.  ,   1.17,
+         0.44], [ 18.3 ,  17.2 ,   0.5 ,  80.64, 400.  ,  68.  , 204.  ,   1.13,
+         0.71], [ 16.21,  12.57,   0.44,  51.89, 481.  ,  26.  , 178.  ,   0.65,
+         0.84], [  8.06,   3.33,   0.96,  81.52, 431.  ,  99.  , 137.  ,   0.17,
+         0.94], [  6.24,  15.06,   0.59,  56.03, 485.  ,  94.  ,  75.  ,   0.99,
+         1.34], [  6.89,  19.71,   0.88,  50.32, 394.  , 146.  , 131.  ,   0.73,
+         0.3 ], [ 18.81,  18.37,   0.68,  59.9 , 381.  , 149.  , 114.  ,   0.55,
+         1.07], [ 11.31,  19.74,   0.62,  70.13, 377.  , 112.  , 178.  ,   0.42,
+         1.1], [ 14.85,   5.12,   0.76,  74.87, 415.  , 112.  , 121.  ,   0.72,
+         1.49], [ 15.61,  21.19,   1.01,  75.49, 435.  , 105.  ,  69.  ,   0.42,
+         0.34], [ 12.51,  25.66,   0.69,  76.69, 477.  ,  40.  , 103.  ,   0.18,
+         0.17], [ 16.28,   3.4 ,   0.86,  54.21, 473.  ,  46.  , 130.  ,   1.2 ,
+         0.51], [ 11.79,   8.83,   1.07,  50.86, 313.  , 130.  , 244.  ,   0.54,
+         0.73], [ 12.19,   6.33,   0.88,  77.72, 352.  , 148.  , 126.  ,   0.18,
+         1.11], [  7.  ,  10.33,   0.68,  71.86, 456.  ,  53.  , 123.  ,   1.34,
+         0.3 ], [  6.66,   6.89,   0.71,  67.71, 403.  ,  23.  , 239.  ,   1.95,
+         0.19]]
+num_offsprings = 80
 offsprings = crossover(parents, num_offsprings)
 child = np.array(offsprings)
 c1 = child
 print("Gen1: Child: \n", c1)
 
+
 # Find out the Fitness Value of Children
 Z1 = []
-for i in range(35):
-    Z1.append(100 - (((c1[i][0] - c1[i][1]) * (100 - c1[i][2])) / c1[i][3]))
+for i in range(80):
+    Z1.append((0.2 * c1[i][0]) + (0.1 * c1[i][1]) + (0.1 * c1[i][2]) + (0.1 * c1[i][3]) + (0.15 * c1[i][4]) + (0.15 * c1[i][5]) + (0.1 * c1[i][6]) + (0.05 * c1[i][7]) + (0.05 * c1[i][8]))
 
 array = np.array(Z1)
-new_array = array.reshape(35, 1)
+new_array = array.reshape(80, 1)
 
 # Find out the Fitness Score of Children of Generation 1 
 def SumofArray(arr):
@@ -108,7 +140,220 @@ def SumofArray(arr):
 ff1 = ((Z1/SumofArray(Z1))*100)
 fs1 = np.array(ff1)
 Y1 = np.round(fs1, 3)
-y1 = Y1.reshape(35, 1)
+y1 = Y1.reshape(80, 1)
+print("Fitness Score:\n", y1)
+
+# Sort the merged array basis on best fitness score
+a1 = [[x, y] for x, y in zip(c1, y1)]
+sorted_a1 = sorted(a1, key=lambda x: x[1], reverse=True)
+print("\n After Sorted basis on Fitness Score:")
+for z1 in sorted_a1:
+    print(z1)
+    
+    
+    
+ # Multipoint Crossover for 2nd Generation-
+
+import numpy as np
+import random
+def crossover(parents, num_offsprings):
+    offsprings = []
+    for i in range(num_offsprings):
+        parent1 = random.choice(parents)
+        parent2 = random.choice(parents)
+        offspring = parent1[:len(parent1)//2] + parent2[len(parent2)//2:]
+        offsprings.append(offspring)
+    return offsprings
+
+parents = [[ 16.28,   3.4 ,   0.86,  54.21, 464.  , 152.  , 213.  ,   0.76,
+         0.62], [ 15.61,  21.19,   1.01,  75.49, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 16.62,  13.64,   0.87,  81.75, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 18.09,   7.77,   0.66,  72.22, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 18.81,  18.37,   0.68,  59.9 , 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 15.61,  21.19,   1.01,  75.49, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.62,  13.64,   0.87,  81.75, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.21,  12.57,   0.44,  51.89, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 12.19,   6.33,   0.88,  77.72, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.21,  12.57,   0.44,  51.89, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.62,  13.64,   0.87,  81.75, 451.  , 108.  , 183.  ,   1.37,
+         1.19], [ 11.79,   8.83,   1.07,  50.86, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.09,  11.63,   0.78,  59.78, 471.  ,  86.  , 191.  ,   0.46,
+         0.39], [  7.67,   7.03,   0.67,  84.59, 451.  , 108.  , 183.  ,   1.37,
+         1.19], [ 15.61,  21.19,   1.01,  75.49, 453.  , 119.  , 137.  ,   0.66,
+         0.61], [ 17.47,  24.14,   0.52,  80.7 , 453.  ,  38.  , 242.  ,   0.16,
+         0.33], [ 16.28,   3.4 ,   0.86,  54.21, 451.  , 108.  , 183.  ,   1.37,
+         1.19], [ 18.81,  18.37,   0.68,  59.9 , 453.  , 119.  , 137.  ,   0.66,
+         0.61], [  6.4 ,  10.13,   0.54,  59.24, 451.  , 108.  , 183.  ,   1.37,
+         1.19], [ 12.19,   6.33,   0.88,  77.72, 453.  , 119.  , 137.  ,   0.66,
+         0.61], [ 15.38,   3.18,   0.7 ,  72.97, 453.  , 119.  , 137.  ,   0.66,
+         0.61], [ 16.09,  11.63,   0.78,  59.78, 453.  , 119.  , 137.  ,   0.66,
+         0.61], [ 18.81,  18.37,   0.68,  59.9 , 453.  ,  38.  , 242.  ,   0.16,
+         0.33], [ 18.81,  18.37,   0.68,  59.9 , 387.  , 129.  , 202.  ,   0.56,
+         0.08], [ 18.09,   7.77,   0.66,  72.22, 387.  , 129.  , 202.  ,   0.56,
+         0.08]]
+num_offsprings = 60
+offsprings = crossover(parents, num_offsprings)
+child = np.array(offsprings)
+c1 = child
+print("Gen2: Child: \n", c1)
+
+
+# Find out the Fitness Value of Children
+Z1 = []
+for i in range(60):
+    Z1.append((0.2 * c1[i][0]) + (0.1 * c1[i][1]) + (0.1 * c1[i][2]) + (0.1 * c1[i][3]) + (0.15 * c1[i][4]) + (0.15 * c1[i][5]) + (0.1 * c1[i][6]) + (0.05 * c1[i][7]) + (0.05 * c1[i][8]))
+
+array = np.array(Z1)
+new_array = array.reshape(60, 1)
+
+# Find out the Fitness Score of Children of Generation 2
+def SumofArray(arr):
+    sum=0
+    n = len(arr)
+    for Z1 in range(n):
+        sum = sum + arr[Z1]
+    return sum
+
+ff1 = ((Z1/SumofArray(Z1))*100)
+fs1 = np.array(ff1)
+Y1 = np.round(fs1, 3)
+y1 = Y1.reshape(60, 1)
+print("Fitness Score:\n", y1)
+
+# Sort the merged array basis on best fitness score
+a1 = [[x, y] for x, y in zip(c1, y1)]
+sorted_a1 = sorted(a1, key=lambda x: x[1], reverse=True)
+print("\n After Sorted basis on Fitness Score:")
+for z1 in sorted_a1:
+    print(z1)
+    
+    
+  
+ # Multipoint Crossover for 3rd Generation-
+    
+    def crossover(parents, num_offsprings):
+    offsprings = []
+    for i in range(num_offsprings):
+        parent1 = random.choice(parents)
+        parent2 = random.choice(parents)
+        offspring = parent1[:len(parent1)//2] + parent2[len(parent2)//2:]
+        offsprings.append(offspring)
+    return offsprings
+
+parents = [[ 16.62,  13.64,   0.87,  81.75, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 18.09,   7.77,   0.66,  72.22, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 18.81,  18.37,   0.68,  59.9 , 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 15.61,  21.19,   1.01,  75.49, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.09,  11.63,   0.78,  59.78, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 18.09,   7.77,   0.66,  72.22, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 18.81,  18.37,   0.68,  59.9 , 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.21,  12.57,   0.44,  51.89, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [  7.67,   7.03,   0.67,  84.59, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 15.38,   3.18,   0.7 ,  72.97, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.09,  11.63,   0.78,  59.78, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 15.61,  21.19,   1.01,  75.49, 471.  ,  86.  , 191.  ,   0.46,
+         0.39], [ 16.28,   3.4 ,   0.86,  54.21, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.62,  13.64,   0.87,  81.75, 451.  , 108.  , 183.  ,   1.37,
+         1.19], [ 15.61,  21.19,   1.01,  75.49, 451.  , 108.  , 183.  ,   1.37,
+         1.19], [ 11.79,   8.83,   1.07,  50.86, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.09,  11.63,   0.78,  59.78, 451.  , 108.  , 183.  ,   1.37,
+         1.19], [ 15.61,  21.19,   1.01,  75.49, 453.  , 119.  , 137.  ,   0.66,
+         0.61], [ 16.21,  12.57,   0.44,  51.89, 451.  , 108.  , 183.  ,   1.37,
+         1.19], [ 18.09,   7.77,   0.66,  72.22, 453.  , 119.  , 137.  ,   0.66,
+         0.61]]
+num_offsprings = 40
+offsprings = crossover(parents, num_offsprings)
+child = np.array(offsprings)
+c1 = child
+print("Gen3: Child: \n", c1)
+
+
+# Find out the Fitness Value of Children
+Z1 = []
+for i in range(40):
+    Z1.append((0.2 * c1[i][0]) + (0.1 * c1[i][1]) + (0.1 * c1[i][2]) + (0.1 * c1[i][3]) + (0.15 * c1[i][4]) + (0.15 * c1[i][5]) + (0.1 * c1[i][6]) + (0.05 * c1[i][7]) + (0.05 * c1[i][8]))
+
+array = np.array(Z1)
+new_array = array.reshape(40, 1)
+
+# Find out the Fitness Score of Children of Generation 3
+def SumofArray(arr):
+    sum=0
+    n = len(arr)
+    for Z1 in range(n):
+        sum = sum + arr[Z1]
+    return sum
+
+ff1 = ((Z1/SumofArray(Z1))*100)
+fs1 = np.array(ff1)
+Y1 = np.round(fs1, 3)
+y1 = Y1.reshape(40, 1)
+print("Fitness Score:\n", y1)
+
+# Sort the merged array basis on best fitness score
+a1 = [[x, y] for x, y in zip(c1, y1)]
+sorted_a1 = sorted(a1, key=lambda x: x[1], reverse=True)
+print("\n After Sorted basis on Fitness Score:")
+for z1 in sorted_a1:
+    print(z1)
+    
+    
+    
+  
+# Multipoint Crossover for 4th Generation-
+
+def crossover(parents, num_offsprings):
+    offsprings = []
+    for i in range(num_offsprings):
+        parent1 = random.choice(parents)
+        parent2 = random.choice(parents)
+        offspring = parent1[:len(parent1)//2] + parent2[len(parent2)//2:]
+        offsprings.append(offspring)
+    return offsprings
+
+parents = [[ 16.62,  13.64,   0.87,  81.75, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 15.61,  21.19,   1.01,  75.49, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 18.81,  18.37,   0.68,  59.9 , 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 16.62,  13.64,   0.87,  81.75, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 15.61,  21.19,   1.01,  75.49, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.09,  11.63,   0.78,  59.78, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 18.09,   7.77,   0.66,  72.22, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 18.81,  18.37,   0.68,  59.9 , 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.21,  12.57,   0.44,  51.89, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [  7.67,   7.03,   0.67,  84.59, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.28,   3.4 ,   0.86,  54.21, 460.  , 153.  , 156.  ,   1.9 ,
+         1.47], [ 16.09,  11.63,   0.78,  59.78, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.21,  12.57,   0.44,  51.89, 486.  , 135.  , 129.  ,   1.59,
+         1.33], [ 16.62,  13.64,   0.87,  81.75, 471.  ,  86.  , 191.  ,   0.46,
+         0.39], [ 15.61,  21.19,   1.01,  75.49, 471.  ,  86.  , 191.  ,   0.46,
+         0.39]]
+num_offsprings = 30
+offsprings = crossover(parents, num_offsprings)
+child = np.array(offsprings)
+c1 = child
+print("Gen4: Child: \n", c1)
+
+
+# Find out the Fitness Value of Children
+Z1 = []
+for i in range(30):
+    Z1.append((0.2 * c1[i][0]) + (0.1 * c1[i][1]) + (0.1 * c1[i][2]) + (0.1 * c1[i][3]) + (0.15 * c1[i][4]) + (0.15 * c1[i][5]) + (0.1 * c1[i][6]) + (0.05 * c1[i][7]) + (0.05 * c1[i][8]))
+
+array = np.array(Z1)
+new_array = array.reshape(30, 1)
+
+# Find out the Fitness Score of Children of Generation 4
+def SumofArray(arr):
+    sum=0
+    n = len(arr)
+    for Z1 in range(n):
+        sum = sum + arr[Z1]
+    return sum
+
+ff1 = ((Z1/SumofArray(Z1))*100)
+fs1 = np.array(ff1)
+Y1 = np.round(fs1, 3)
+y1 = Y1.reshape(30, 1)
 print("Fitness Score:\n", y1)
 
 # Sort the merged array basis on best fitness score
